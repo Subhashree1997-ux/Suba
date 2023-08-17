ddb5d258e77c865f2176e39f45c71eb2db5d3d3d
Subhashree1997-ux <subamuki1997@gmail.com>
2023-08-17 12:17:35 +0530

    changed by dev

--- src/org/task/College1.java
+++ src/org/task/College1.java
@@ -8,6 +8,7 @@ public class College1 extends College {
 	public void courseName1() {
 		this.studentName1();
 		System.out.println("The Course Name:B.E.,ECE");
+		System.out.println("code developed by developer");
 	}
 
 }
